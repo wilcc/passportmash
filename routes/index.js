6ee0233da1f99fc94cfb0381f2ac809661397c21
Wilson Chen <wilson.chen@codeimmersives.com>
2020-06-18 10:42:59 -0400

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controller/controller')
 
+
+
+router.use((req,res,next)=>{
+    res.locals.user = req.user
+    res.locals.errors =req.flash('errors')
+    res.locals.success = req.flash('success')
+    next()
+  })
+  
 router.get('/movies', controller.movies)
 router.get('/random', controller.random)
 
